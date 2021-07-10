4d004d9dabd5a705d931b31e7316d80249fb87ad
Espartaco Palma <git@esparta.co>
2021-07-10 15:12:28 -0700

    Adding origin name to the list of repos
    This is allowing us to specify the origin name while clonning,
    so we can use any arbitrary name. And use it on the commands
    as:
    
        git push github HEAD
    
    Currently all the repos are from github remote, but that might
    change.

--- src/repos.sh
+++ src/repos.sh
@@ -4,19 +4,19 @@
 # and fetch it locally.
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 echo "I $(date +"%F %T") - Preparing the repos"
-while IFS=, read -r branch origin target upstream
+while IFS=, read -r origin_name branch repo_url target upstream
 do
   target_directory="${HOME}/$target"
-  echo "I $(date +"%F %T") - Starting clonning $origin"
-  if $(git clone $origin $target_directory &> /dev/null) ; then
-    echo "I $(date +"%F %T") - Done with $origin"
+  echo "I $(date +"%F %T") - Starting clonning $repo_url"
+  if $(git clone --origin $origin_name $repo_url $target_directory &> /dev/null) ; then
+    echo "I $(date +"%F %T") - Done with $repo_url"
     if [ ! -z "$target" ]; then
       cd "$target_directory"
       git remote add upstream $upstream
       git pull upstream $branch
     fi
   else
-    echo "Skiping $origin"
+    echo "Skiping $repo_url"
   fi
 done < "$SCRIPT_PATH/repos/list.csv"
 
